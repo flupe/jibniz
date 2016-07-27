a4898c181e47de75fe4ed12ff18d973b147f5d45
flupe <baboum@acatalepsie.fr>
2016-07-27 04:30:19 +0200

    new method, incomplete

--- main.js
+++ main.js
@@ -4,93 +4,66 @@
 var raf = window.requestAnimationFrame
 var J = window.jibniz = {}
 
-// todo:
+// VVUU.YYYY
 var YUVtoHEX = c => {
-  var v = c >> 8 & 0xff
-  return 0xff000000 | v << 16 | v << 8 | v
-}
-
-// probably useless
-function Stack(constructor, size) {
-  this.data = new constructor(size)
-  this.size = size
-  this.next = 0
-}
-
-Stack.prototype = {
-  push: function(x) {
-    this.data[this.next] = x
-    this.next = (this.next + 1) % this.size
-  },
-
-  pop: function(x) {
-    this.next = (this.next == 0) ? this.size - 1 : this.next - 1
-    return this.data[this.next]
-  },
-
-  top: function() {
-    return this.data[(this.next == 0) ? this.size - 1 : this.next - 1]
-  }
+  var l = (c >> 8) & 0xff
+  return 0xff000000 | l << 16 | l << 8 | l
 }
 
 J.Console = function() {
   var cvs = this.domElement = document.createElement('canvas')
   var ctx = cvs.getContext('2d')
-  var stack  = this.stack = new Stack(Int32Array, 65536)
-  var rstack = this.rstack = new Stack(Int32Array, 65536)
+
+  // hahaha, lightweight… right
+  var MEM     = new Int32Array(1048576)
+  var USRDATA = new Int32Array(MEM.buffer, 0, 786432)
+  var ARSTACK = new Int32Array(MEM.buffer, 102400, 16384)
+  var VRSTACK = new Int32Array(MEM.buffer, 104448, 16384)
+  var ASTACK  = new Int32Array(MEM.buffer, 106496, 65536)
+  var VSTACK  = new Int32Array(MEM.buffer, 114688, 131072)
+
+  var pointers = {
+    vs: 0,
+    as: 0,
+    vr: 0,
+    ar: 0
+  }
 
   cvs.width = cvs.height = 256
   var imageData = ctx.createImageData(256, 256)
   var buf32 = new Uint32Array(imageData.data.buffer)
 
-
-
   this.time = 0
   // position of the current fragment
   var x = 0, y = 0
-
-  // later, MEM
 
   this.run = function() {
     raf(this.step)
   }
 
   this.step = function() {
-    raf(this.step)
+    // raf(this.step)
     for (y = 0; y < 256; y++) {
       for(x = 0; x < 256; x++) {
         this.whereami()
-        this.program(this)
+        this.program(VSTACK, pointers)
       }
     }
 
+    var offset = pointers.vs < 65536 ? 65536 : 0
     for (var i = 65536; i--;)
-      buf32[i] = YUVtoHEX(stack.data[i])
+      buf32[i] = YUVtoHEX(VSTACK[offset + i])
 
     ctx.putImageData(imageData, 0, 0)
     this.time ++
   }.bind(this)
 
-  this.add = _ => {stack.push(stack.pop() + stack.pop())}
-  this.sub = _ => {stack.push(-stack.pop() + stack.pop())}
-  this.mul = _ => {stack.push(stack.pop() * stack.pop() >> 16)}
-  this.div = _ => {
-    var b = stack.pop()
-    var a = stack.pop()
-    stack.push(((a << 16) / b) | 0)
+  // tmp: to be deleted
+  function push(x) {
+    VSTACK[pointers.vs] = x
+    pointers.vs = pointers.vs + 1 & 131071
   }
-  this.mod = _ => {
-    var b = stack.pop()
-    var a = stack.pop()
-    stack.push(b == 0 ? 0 : a % b)
-  }
-  this.sqrt = _ => {
-    var a = stack.pop()
-    stack.push(a > 0 ? Math.sqrt(stack.pop() / 65536) * 65536 | 0 : 0)
-  }
-  this.and = _ => {stack.push(stack.pop() & stack.pop())}
-  this.or = _ => {stack.push(stack.pop() | stack.pop())}
-  this.xor = _ => {stack.push(stack.pop() ^ stack.pop())}
+
   this.right = _ => {
     var b = stack.pop() >> 16
     var a = stack.pop()
@@ -98,41 +71,7 @@ J.Console = function() {
     var z =   a << (32 - b)
     stack.push(y | z)
   }
-  this.left = _ => {
-    var b = stack.pop() >> 16
-    stack.push(stack.pop() << b)
-  }
-  this.neg = _ => {stack.push(~stack.pop())}
-  this.sin = _ => {stack.push(Math.sin(stack.pop() / 65536) * 65536 | 0)}
-  this.atan = _ => {
-    var b = stack.pop() >> 16
-    stack.push(Math.atan(stack.pop() >> 16, b) / (2 * Math.PI) * 65536 | 0)
-  }
-  this.isneg = _ => {
-    var a = stack.pop()
-    stack.push(a < 0 ? a : 0)
-  }
-  this.ispos = _ => {
-    var a = stack.pop()
-    stack.push(a > 0 ? a : 0)
-  }
-  this.iszero = _ => {stack.push(stack.pop() == 0 ? 1 : 0)} // maybe automatically casted?
-  this.dup = _ => {stack.push(stack.top())}
-  this.pop = _ => {stack.pop()}
-  this.exchange = _ => {
-    var b = stack.pop()
-    var a = stack.pop()
-    stack.push(b)
-    stack.push(a)
-  }
-  this.trirot = _ => {
-    var c = stack.pop()
-    var b = stack.pop()
-    var a = stack.pop()
-    stack.push(b)
-    stack.push(c)
-    stack.push(a)
-  }
+
   this.if = _ => stack.pop() != 0
 
   this.retaddr = _ => {stack.push(rstack.pop())}
@@ -147,48 +86,96 @@ J.Console = function() {
   }
 
   this.whereami = _ => {
-    // for now, assuming video mode;
-    stack.push(this.time << 16)
-    stack.push(coord(y))
-    stack.push(coord(x))
+    // for now, assuming video mode
+    push(this.time << 16)
+    push(coord(y))
+    push(coord(x))
   }
 
-  // later
-  this.pick = _ => {}
-  this.bury = _ => {}
+  this.pick = _ => {
+    var i = stack.pop()
+    if (stack.next > i)
+      stack.push(stack.data[i])
+    else
+      stack.push(stack.data[stack.next + stack.size - i])
+  }
+  this.bury = _ => {
+    var i = stack.pop()
+    var val = stack.pop()
+    if (stack.next > i)
+      stack.data[i] = val
+    else
+      stack.data[stack.next + stack.size - i] = val
+  }
   this.load = _ => {}
   this.store = _ => {}
   this.index = _ => {}
   this.outdex = _ => {}
 }
 
-// trivial ops
+var vsincr = 'p.vs=p.vs+1&131071;'
+var vsdecr = 'p.vs=p.vs+131071&131071;'
+
 var codes = {
-  '+': 'c.add();',
-  '-': 'c.sub();',
-  '*': 'c.mul();',
-  '/': 'c.div();',
-  '%': 'c.mod();',
-  'q': 'c.sqrt();',
-  '&': 'c.and();',
-  '|': 'c.or();',
-  '^': 'c.xor();',
+  '+': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]+VS[p.vs];',
+  '-': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]-VS[p.vs];',
+  '*': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]*VS[p.vs]>>16;',
+  '/': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[p.vs]==0?0:(VS[a]<<16)/VS[p.vs];',
+  '%': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[p.vs]==0?0:VS[a]%VS[p.vs];',
+  'q': 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]>0?Math.sqrt(VS[a]/65536)*65536:0;',
+  '&': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]&VS[p.vs];',
+  '|': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]|VS[p.vs];',
+  '^': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]^VS[p.vs];',
   'r': 'c.right();',
-  'l': 'c.left();',
-  '~': 'c.neg();',
-  's': 'c.sin();',
-  'a': 'c.atan();',
-  '<': 'c.isneg();',
-  '>': 'c.ispos();',
-  '=': 'c.iszero();',
-  'd': 'c.dup();',
-  'p': 'c.pop();',
-  'x': 'c.exchange();',
-  'v': 'c.trirot();',
+  'l': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]<<(VS[p.vs]>>16);',
+  '~': 'a=p.vs+131071&131071;'
+     + 'VS[a]=~VS[a];',
+  's': 'a=p.vs+131071&131071;'
+     + 'VS[a]=Math.sin(VS[a]/65536)*65536;',
+  'a': vsdecr
+     + 'a=p.vs+131071&131071;'
+     + 'VS[a]=Math.atan(VS[a]/65536,VS[p.vs]/65536)/(2*Math.PI)*65536;',
+  '<': 'a=p.vs+131071&131071;'
+     + 'if(VS[a]>0)VS[a]=0;',
+  '>': 'a=p.vs+131071&131071;'
+     + 'if(VS[a]<0)VS[a]=0;',
+  '=': 'a=p.vs+131071&131071;'
+     + 'VS[a]=VS[a]==0;',
+  'd': 'VS[p.vs]=VS[p.vs+131071&131071];'
+     + vsincr,
+  'p': vsdecr,
+  'x': 'a=p.vs+131071&131071;'
+     + 'b=a+131071&131071;'
+     + 'c=VS[a];VS[a]=VS[b];VS[b]=c;',
+  'v': 'a=p.vs+131071&131071;'
+     + 'b=a+131071&131071;'
+     + 'c=b+131071&131071;'
+     + 'd=VS[a];VS[a]=VS[c];VS[c]=VS[b];VS[b]=d;',
+
+  // todo:
   ')': 'c.pick();',
   '(': 'c.bury();',
   'w': 'c.whereami();',
-  'T': 'return;',
+  'T': 'break;',
   '@': 'c.load();',
   '!': 'c.store();',
   'i': 'c.index();',
@@ -211,17 +198,26 @@ var isHexaDecimal = c => {
                    || c == 'D' || c == 'E' || c == 'F'
 }
 
-window.isHex = isHexaDecimal
+var isBlank = c => c == ' ' || c == ',' || c == ';' || c == '\n'
 
 function next(state) {
   var c = state.src[state.pos++]
 
-  if (c == ' ' || c == ',' || c == ';' || c == '\n')
+  if (isBlank(c))
     return
 
-  if (codes[c]) {
-    state.body += codes[c]
+  if (c == '\\') {
+    while (state.pos < state.len && state.src[state.pos++] != '\n') {}
+    return
   }
+
+  state.body += '\ncase ' + (state.inst++) + ':\n'
+
+  if (codes[c]) {
+    state.body += codes[c] + '\n'
+  }
+
+  // todo: take fractional part into account
   else if (isHexaDecimal(c)) {
     var integer = parseInt(c, 16) << 16
     while(state.pos < state.len && isHexaDecimal(state.src[state.pos]))
@@ -229,24 +225,20 @@ function next(state) {
     state.body += 'c.loadimm('+integer+');'
   }
   else {
-    // comment
-    switch(c) {
-      case '\\':
-        while (state.pos < state.len && state.src[state.pos++] != '\n') {}
-        break
-      case '?': // if
-        state.body += 'if(c.if()) {' // not pretty, but eh
-        eatUntil(state, isEndIf)
-        state.body += '}'
-        break
-      case ':': // else
-        // to improve, regarding error handling
-        state.body += ' else {'
-        eatUntil(state, isEndElse)
-        state.body += '}'
-        break
-      // todo: subroutines and such
-    }
+    // switch(c) {
+    //   case '?': // if
+    //     state.body += 'if(c.if()) {' // not pretty, but eh
+    //     eatUntil(state, isEndIf)
+    //     state.body += '}'
+    //     break
+    //   case ':': // else
+    //     // to improve, regarding error handling
+    //     state.body += ' else {'
+    //     eatUntil(state, isEndElse)
+    //     state.body += '}'
+    //     break
+    //   // todo: subroutines and such
+    // }
   }
 }
 
@@ -254,14 +246,25 @@ J.compile = function(src) {
   var state = {
     src,
     pos: 0,
+    inst: 0,
     len: src.length,
-    body: ''
+    body: '',
   }
+
+  state.body += 'var loop = true;\n'
+  state.body += 'var inst = 0;\n'
+  state.body += 'var a, b, c, d; \n'
+  state.body += 'while(loop) {\n'
+  state.body += '\tswitch(inst) {\n'
 
   while (state.pos < state.len)
     next(state)
 
-  return new Function('c', state.body)
+  state.body += '\n\t}\n'
+  state.body += '\tloop = false;\n'
+  state.body += '}'
+
+  return new Function('VS', 'p', state.body)
 }
 
 })()
