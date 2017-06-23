4678da6486174208da2c4de6d2506910e03ef9f2
flupe <lucas@escot.me>
2017-06-23 21:51:58 +0200

    draft of more flexible instr definition

--- analysis.js
+++ analysis.js
@@ -1,137 +1,7 @@
 (function() {
 'use strict'
 
-let raf = window.requestAnimationFrame
 let J = window.jibniz = {}
-
-// VVUU.YYYY
-let YUVtoHEX = c => {
-  let y = c >> 8 & 0xff
-  return 0xff000000 | y << 16 | y << 8 | y
-}
-
-J.Console = function() {
-  let cvs = this.domElement = document.createElement('canvas')
-  let ctx = cvs.getContext('2d')
-
-  let MEM     = new Int32Array(1048576)
-  let USRDATA = new Int32Array(MEM.buffer, 0, 786432)
-  let ARSTACK = new Int32Array(MEM.buffer, 102400, 16384)
-  let VRSTACK = new Int32Array(MEM.buffer, 104448, 16384)
-  let ASTACK  = new Int32Array(MEM.buffer, 106496, 65536)
-  let VSTACK  = new Int32Array(MEM.buffer, 114688, 131072)
-
-  let audio = { S: ASTACK, sn: 0, sm:  65535, R: ARSTACK, rn: 0, rm: 16383 }
-  let video = { S: VSTACK, sn: 0, sm: 131071, R: VRSTACK, rn: 0, rm: 16383 }
-
-  cvs.width = cvs.height = 256
-
-  let imageData = ctx.createImageData(256, 256)
-  let buf32 = new Uint32Array(imageData.data.buffer)
-
-  this.time = 0
-  // for now, by default and until we do proper mode detection
-  this.tyx = true
-
-  // position of the current fragment
-  let x = 0
-  let y = 0
-
-  let mouseX = 0
-  let mouseY = 0
-  let ctrlKey = 0
-  let altKey = 0
-  let click = 0
-
-  this.run = function() {
-    raf(this.step)
-  }
-
-  this.step = function() {
-    raf(this.step)
-
-    let w = this.tyx ? whereami_tyx : whereami_t
-    let U = mouseY << 24
-          | mouseX << 16
-          | click << 15
-          | ctrlKey << 14
-          | altKey << 13
-
-    for (y = 0; y < 256; y++) {
-      for(x = 0; x < 256; x++) {
-        w(video.sn)
-        this.program(video, MEM, w, U)
-      }
-    }
-
-    let offset = video.sn < 65536 ? 65536 : 0
-    for (let i = 65536; i--;)
-      buf32[i] = YUVtoHEX(VSTACK[offset + i])
-
-    ctx.putImageData(imageData, 0, 0)
-    this.time++
-  }.bind(this)
-
-  // tmp: to be deleted
-  function push(x) {
-    VSTACK[video.sn] = x
-    video.sn = video.sn + 1 & video.sm
-  }
-
-  let coord = v => v - 128 << 9
-
-  console.log(coord(256).toString(16))
-
-  let whereami_tyx = (sn) => {
-    video.sn = sn
-    push(this.time << 16)
-    push(coord(y))
-    push(coord(x))
-    return video.sn
-  }
-
-  let whereami_t = (sn) => {
-    video.sn = sn
-    push(this.time << 16 | y << 8 | x)
-    return video.sn
-  }
-
-  // in the future, see if using the Pointer API is worthwhile
-  cvs.addEventListener('mousemove', e => {
-    let x = e.pageX
-    let y = e.pageY
-
-    // substract canvas offset
-    let elem = cvs
-    do {
-      x -= elem.offsetLeft
-      y -= elem.offsetTop
-    } while(elem = elem.offsetParent)
-
-    mouseX = x
-    mouseY = y
-  })
-
-  cvs.addEventListener('mousedown', e => {
-    click = 1
-  })
-
-  cvs.addEventListener('mouseup', e => {
-    click = 0
-  })
-
-  window.addEventListener('keydown', e => {
-    ctrlKey = e.ctrlKey | 0
-    altKey = e.altKey | 0
-
-    // TODO: also support keys
-  })
-
-  window.addEventListener('keyup', e => {
-    ctrlKey = e.ctrlKey | 0
-    altKey = e.altKey | 0
-  })
-}
 
 // increase/decrease stack pointer
 let sincr = 'sn=sn+1&sm;'
@@ -141,98 +11,54 @@ let sdecr = 'sn=sn+sm&sm;'
 let rincr = 'rn=rn+1&rm;'
 let rdecr = 'rn=rn+rm&rm;'
 
+/*
+ * nbst: nb of variables taken on value stack
+ * nbrt: nb of variables taken on return stack
+ * spv: values pushed on value stack
+ * rpv: values pushed on return stack
+ * action
+ */
+
+function make_instr([ nbst, nbrt, code, spv, rpv ]) {
+  return { nbst, nbrt, code, spv, rpv }
+}
+
+let instructions = {}
+
+// $n ... $2 $1 --
+// #n ... #2 #1 --
+
+;[[ '+', 2, 0, '', ['$1+$2'], [] ],
+  [ '-', 2, 0, '', ['$1-$2'], [] ],
+  [ '*', 2, 0, '', ['($1/65536)*($2/65536)*65536|0'], [] ],
+  [ '/', 2, 0, '', ['$1==0?0:$2*65536/$1'], [] ],
+  [ '%', 2, 0, '', ['$1==0?0:$2%$1'], [] ],
+
+  [ 'q', 1, 0, '', ['$1>0?Math.sqrt($1/65536)*65536|0:0'], [] ],
+
+  [ '&', 2, 0, '', ['$1&$2'], [] ],
+  [ '|', 2, 0, '', ['$1|$2'], [] ],
+  [ '^', 2, 0, '', ['$1^$2'], [] ],
+  [ 'r', 2, 0, 'a=$1>>>16;', ['($2>>>a)|($2<<(16-a))'], [] ],
+  [ 'l', 2, 0, '', ['$2<<($1>>>16)'], [] ],
+  [ '~', 1, 0, '', ['~$1'], [] ],
+
+  [ 's', 1, 0, '', ['Math.sin($1*Math.PI/32768)*65536|0'], [] ],
+  [ 'a', 2, 0, '', ['Math.atan2($1/65536,$2/65536)/Math.PI*32768|0'], [] ],
+
+  [ '<', 1, 0, '', ['$1>0?0:$1'], [] ],
+  [ '>', 1, 0, '', ['$1<0?0:$1'], [] ],
+  [ '=', 1, 0, '', ['($1==0)<<16'], [] ],
+
+  [ 'd', 1, 0, '', ['$1', '$1'], [] ],
+  [ 'p', 1, 0, '', [], [] ],
+  [ 'x', 2, 0, '', ['$1', '$2'], [] ],
+  [ 'v', 3, 0, '', ['$2', '$1', '$3'], [] ],
+].map(args => {
+  instructions[args.pop()] = make_instr(args)
+})
+
 let codes = {
-
-  // ARITHMETIC
-
-  '+': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]+S[sn];',
-
-  '-': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]-S[sn];',
-
-  '*': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=(S[a]/65536)*(S[sn]/65536)*65536|0;',
-
-  '/': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[sn]==0?0:(S[a]*65536)/S[sn];',
-
-  '%': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[sn]==0?0:S[a]%S[sn];',
-
-  // square root
-  'q': 'a=sn+sm&sm;'
-     + 'S[a]=S[a]>0?Math.sqrt(S[a]/65536)*65536|0:0;',
-
-  '&': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]&S[sn];',
-
-  '|': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]|S[sn];',
-
-  '^': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]^S[sn];',
-
-  // rotate shift
-  'r': sdecr
-     + 'a=sn+sm&sm;'
-     + 'b=S[sn]>>>16;'
-     + 'c=S[a];'
-     + 'S[a]=(c>>>b)|(c<<(16-c));',
-
-  'l': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=S[a]<<(S[sn]>>>16);',
-
-  '~': 'a=sn+sm&sm;'
-     + 'S[a]=~S[a];',
-
-  // sin
-  's': 'a=sn+sm&sm;'
-     + 'S[a]=Math.sin(S[a]*Math.PI/32768)*65536;',
-
-  // atan
-  'a': sdecr
-     + 'a=sn+sm&sm;'
-     + 'S[a]=Math.atan2(S[sn]/65536,S[a]/65536)/Math.PI*32768;',
-
-  '<': 'a=sn+sm&sm;'
-     + 'if(S[a]>0)S[a]=0;',
-
-  '>': 'a=sn+sm&sm;'
-     + 'if(S[a]<0)S[a]=0;',
-
-  '=': 'a=sn+sm&sm;'
-     + 'S[a]=(S[a]==0)<<16;',
-
-
-  // STACK MANIPULATION
-
-  // dup: a -- a a
-  'd': 'S[sn]=S[sn+sm&sm];'
-     + sincr,
-
-  // pop: a --
-  'p': sdecr,
-
-  // exchange: a b -- b a
-  'x': 'a=sn+sm&sm;'
-     + 'b=a+sm&sm;'
-     + 'c=S[a];S[a]=S[b];S[b]=c;',
-
-  // trirot: a b c -- b c a
-  'v': 'a=sn+sm&sm;'
-     + 'b=a+sm&sm;'
-     + 'c=b+sm&sm;'
-     + 'd=S[a];S[a]=S[c];S[c]=S[b];S[b]=d;',
 
   // pick: i -- val
   // TODO: wrap within stack range
